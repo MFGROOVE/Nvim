c3d3ef6bae474abca2bddad12f751d84ade3b507
Marcelo <marcelogulart69@gmail.com>
2026-02-23 19:51:27 -0300

    .

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,12 +3,28 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		cmd = "Neotree",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
 		opts = {
 			close_if_last_window = true,
 			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+				use_libuv_file_watcher = true,
+			},
+			window = {
+				mappings = {
+					["<tab>"] = "toggle_node",
+				},
+			},
 		},
 	},
 }
